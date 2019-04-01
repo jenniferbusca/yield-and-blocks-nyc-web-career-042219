9a5455ef9b8566d4528576aadd1b659585c44514
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-01 18:46:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,4 +7,10 @@ def hello_t(array)
   end
 end
 
-def hello_t(["Tim", "Tom", "Jim"])
+hello_t(["Tim", "Tom", "Jim"])
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
